9f57ddccca2c33a12f49e3eb13bdfdb8abf093d9
cardoso <matheus@cardo.so>
2018-07-15 18:11:54 -0300

    🎉 initial commit

---
+++ SwiftChatClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftChatClient
+//
+//  Created by Matheus Cardoso on 7/15/18.
+//  Copyright © 2018 cardoso. All rights reserved.
+//
+
+import UIKit
+import SwiftChatCore
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
